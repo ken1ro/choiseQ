f15ff4d4b65f3e4b4aecca975236a8a0b34d5ca0
Ken'ichiro Yagura <kenkenkenpa@gmail.com>
2013-05-06 16:36:27 +0900

    Initial Commit

---
+++ choiseQTests/choiseQTests.h
@@ -0,0 +1,13 @@
+//
+//  choiseQTests.h
+//  choiseQTests
+//
+//  Created by Ken Yagura on 13/05/06.
+//  Copyright (c) 2013年 Ken Yagura. All rights reserved.
+//
+
+#import <SenTestingKit/SenTestingKit.h>
+
+@interface choiseQTests : SenTestCase
+
+@end
---
+++ choiseQTests/choiseQTests.m
@@ -0,0 +1,32 @@
+//
+//  choiseQTests.m
+//  choiseQTests
+//
+//  Created by Ken Yagura on 13/05/06.
+//  Copyright (c) 2013年 Ken Yagura. All rights reserved.
+//
+
+#import "choiseQTests.h"
+
+@implementation choiseQTests
+
+- (void)setUp
+{
+    [super setUp];
+    
+    // Set-up code here.
+}
+
+- (void)tearDown
+{
+    // Tear-down code here.
+    
+    [super tearDown];
+}
+
+- (void)testExample
+{
+    STFail(@"Unit tests are not implemented yet in choiseQTests");
+}
+
+@end
