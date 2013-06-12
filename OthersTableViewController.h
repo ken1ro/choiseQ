9163aa57d26ad728a271bc6265bc4ebfb3e0662d
Ken'ichiro Yagura <kenkenkenpa@gmail.com>
2013-06-13 00:28:40 +0900

    20130612　Othersページ作成中

---
+++ OthersTableViewController.h
@@ -0,0 +1,13 @@
+//
+//  OthersTableViewController.h
+//  choiseQ
+//
+//  Created by Ken Yagura on 13/06/13.
+//  Copyright (c) 2013年 Ken Yagura. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface OthersTableViewController : UITableViewController
+
+@end
